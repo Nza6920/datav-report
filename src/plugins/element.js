8b61ec87711ffc66c845d48eb0b1b94dceee4858
Nza6920 <1484663282@qq.com>
2020-11-08 15:32:12 +0800

    add sales-view

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
-import { Card, Col, Row } from 'element-ui'
+import { Card, Col, DatePicker, Menu, MenuItem, RadioButton, RadioGroup, Row } from 'element-ui'
 
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(DatePicker)
